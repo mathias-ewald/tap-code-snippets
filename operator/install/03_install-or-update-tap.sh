df03e923a2e59eb12ca5533f9bedebf1d2981fec
Mathias Ewald <mewald@edit.training>
2022-02-18 22:12:55 +0100

    update

--- operator/install/03_install-or-update-tap.sh
+++ operator/install/03_install-or-update-tap.sh
@@ -6,12 +6,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 NAMESPACE="tap-install"
 TAP_VERSION="1.0.1"
 
-
 if [ ! -f $SCRIPT_DIR/config.yaml ]; 
   echo "File config.yaml does not exist. Copy from config-extample.yaml and modify for your environment."
   exit 1
 fi 
-
 
 function check_install_or_upgrade () {
     OUTPUT="$(tanzu package installed get -n "$NAMESPACE" tap 2>&1)"
@@ -22,12 +20,14 @@ function check_install_or_upgrade () {
     echo "upgrade"
 }
 
+# Generate the final values.yaml for the TAP package
+ytt -f values-template.yaml -f config.yaml > values.yaml
+
+# Install or update the TAP package
 ACTION="install"
 if [ "$(check_install_or_upgrade)" == 'upgrade' ]; then
   ACTION="installed update"
 fi
-
-ytt -f values-template.yaml -f config.yaml > values.yaml
 
 tanzu package $ACTION tap \
   -p tap.tanzu.vmware.com \
