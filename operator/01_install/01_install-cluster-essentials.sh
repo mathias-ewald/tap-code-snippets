d05e2f8cffab0213ef0e498dd07b92fda8b3679d
Mathias Ewald <mewald@edit.training>
2022-02-19 15:41:48 +0100

    update

--- operator/01_install/01_install-cluster-essentials.sh
+++ operator/01_install/01_install-cluster-essentials.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+INSTALL_BUNDLE=${INSTALL_BUNDLE}
+INSTALL_REGISTRY_HOSTNAME=${INSTALL_REGISTRY_HOSTNAME}
+INSTALL_REGISTRY_USERNAME=${INSTALL_REGISTRY_USERNAME}
+INSTALL_REGISTRY_PASSWORD=${INSTALL_REGISTRY_PASSWORD}
 DOWNLOADS_DIR="$SCRIPT_DIR/downloads"
 
 # Check if Tanzu Cluster Essentials bundle is available
