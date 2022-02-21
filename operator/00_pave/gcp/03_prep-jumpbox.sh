c4231708abe34b79991f608fa6621bff35d33e6c
Mathias Ewald <mewald@edit.training>
2022-02-21 12:55:49 +0100

    update

---
+++ operator/00_pave/gcp/03_prep-jumpbox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+PROJECT_ID="cso-pcfs-emea-mewald"
+REGION="europe-west3"
+
+gcloud auth login
+gcloud config set project $PROJECT_ID
+
+gcloud container clusters get-credentials tap-cluster-demo \
+  --region $REGION \
+  --project $PROJECT_ID
