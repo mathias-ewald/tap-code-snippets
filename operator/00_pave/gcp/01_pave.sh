a83df73a3ed0c8ef1bee61a4fb405be29f30509b
Mathias Ewald <mewald@edit.training>
2022-02-22 09:21:34 +0100

    update

--- operator/00_pave/gcp/01_pave.sh
+++ operator/00_pave/gcp/01_pave.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-
-PROJECT_ID=${PROJECT_ID}
-ENVIRONMENT_NAME=${ENVIRONMENT_NAME}
-REGION="${REGION}"
-export GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS}
-
-export TF_VAR_region="$REGION"
-export TF_VAR_project_id="$PROJECT_ID"
-export TF_VAR_environment="$ENVIRONMENT_NAME"
-export TF_VAR_zones="[\"${REGION}-a\", \"${REGION}-b\", \"${REGION}-c\"]"
-
-cat <<EOF > backend.config
-bucket = "tap-${ENVIRONMENT_NAME}-tfstate"
-prefix = "tap-tfstate"
-EOF
+source ./tf-common.sh
 
 terraform init -backend-config=backend.config
 terraform apply
