92417d8ed960cb3d9ecb65d8468b0838f1115f5f
Mathias Ewald <mewald@edit.training>
2022-03-29 09:01:51 +0200

    Fix formatting

--- operator/00_pave/gcp/00_prep.sh
+++ operator/00_pave/gcp/00_prep.sh
@@ -11,10 +11,12 @@ gsutil ls | grep $BUCKET_NAME > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Bucket $BUCKET_NAME already exists."
 else
-    gsutil mb \
-        -p "$PROJECT_ID" \
-        -c "STANDARD" \
-        -l "$REGION" \
-        -b on \
-        "gs://tap-$ENVIRONMENT_NAME-tfstate"
+  BUCKET_NAME="gs://tap-$ENVIRONMENT_NAME-tfstate"
+  echo "Creating bucket $BUCKET_NAME"
+  gsutil mb \
+    -p "$PROJECT_ID" \
+    -c "STANDARD" \
+    -l "$REGION" \
+    -b on \
+    $BUCKET_NAME
 fi
