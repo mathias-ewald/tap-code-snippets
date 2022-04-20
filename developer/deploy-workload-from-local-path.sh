a1847ff47a965844d0f6531920077d4272eb6f0d
Mathias Ewald <mathias.ewald@googlemail.com>
2022-04-20 08:45:43 +0000

    fix

--- developer/deploy-workload-from-local-path.sh
+++ developer/deploy-workload-from-local-path.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 set -euxo pipefail
 
-ACCELERATOR_API="accelerator.tap.halusky.net"
+ACCELERATOR_API="http://accelerator.tap.halusky.net"
 ACCELERATOR_NAME="spring-sensors-rabbit"
-
-PROJECT_NAME="myapp"
-PROJECT_GITREPO="git@github.com/mathias-ewald/myapp"
-PROJECT_GITBRANCH="main"
+NAMESPACE="mewald"
 
 # Download accelerator as zip file
 tanzu accelerator generate $ACCELERATOR_NAME \
-    --server-url $ACCELERATOR_API \
-    --options='{"gitUrl": "'"$PROJECT_GITREPO"'","gitBranch": "'"$PROJECT_GITBRANCH"'"}'
+    --server-url $ACCELERATOR_API
 
 # Unpack the accelerator
 unzip ${ACCELERATOR_NAME}.zip
 
 # Create workload
-tanzu app workload create $PROJECT_NAME \
+tanzu app workload create $ACCELERATOR_NAME \
+    --namespace $NAMESPACE \
     --local-path $ACCELERATOR_NAME \
     --source-image gcr.io/cso-pcfs-emea-mewald/local-build-stage \
-    --label "apps.tanzu.vmware.com/workload-type=web"
+    --label "apps.tanzu.vmware.com/workload-type=web" \
+    --label "app.kubernetes.io/part-of=$ACCELERATOR_NAME"
 
 echo "##################################################################
-Watch Status:    tanzu app workload get --namespace $NAMESPACE $PROJECT_NAME
-Tail Logs:       tanzu app workload tail $PROJECT_NAME
+Watch Status:    tanzu app workload get --namespace $NAMESPACE $ACCELERATOR_NAME
+Tail Logs:       tanzu app workload tail $ACCELERATOR_NAME
 Observe Pods:    kubectl -n $NAMESPACE get pods
 ##################################################################"
