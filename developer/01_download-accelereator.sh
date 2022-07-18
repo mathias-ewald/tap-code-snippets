4c9c2d0dee3fde7f6ba131951301e50c48ecc65a
Mathias Ewald <mathias.ewald@googlemail.com>
2022-07-18 16:44:37 +0000

    update

--- developer/01_download-accelereator.sh
+++ developer/01_download-accelereator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-ACCELERATOR_API="http://accelerator.tap.halusky.net"
-ACCELERATOR_NAME="spring-sensors-rabbit"
+ACCELERATOR_API="https://accelerator.tap.halusky.net"
+ACCELERATOR_NAME="tanzu-java-web-app"
 
 # Download accelerator as zip file
 tanzu accelerator generate $ACCELERATOR_NAME \
